eeeb0c96fa34a4e5520bf99ab83cb2975ba3fc74
Nelson Wu <Nelsonwu820@gmail.com>
2024-02-14 18:15:28 -0500

    changeing res.json to arr to test what is being sent to frontend

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
 
 app.get('*', (req, res) => {
-  res.json("Neat")
+  res.json(["one", "two"])
 });
 
 //for vercel to turn Express into serveless function
