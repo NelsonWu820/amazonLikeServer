bb05043a04a00d333852d55c2ffc3093dbc46343
Nelson Wu <Nelsonwu820@gmail.com>
2024-02-14 18:13:28 -0500

    changing app.get pathing

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.json("Neat")
 });
 
